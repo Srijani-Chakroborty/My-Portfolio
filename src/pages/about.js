8da6635ae0953f55dfae4f5196e47c50d1c82307
Srijani Chakroborty <85583566+Srijani-Chakroborty@users.noreply.github.com>
2024-01-18 00:15:48 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -55,7 +55,7 @@ every project I work on. I look forward to the opportunity to bring my skills an
         </div>
     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-light'/>
-        <Image src={profilePic} als="srijani" className='w-full h-auto rounded-2xl'  priority
+        <Image src={profilePic} alt="srijani" className='w-full h-auto rounded-2xl'  priority
             sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw" />
         </div>
         <div className='col-span-2 flex flex-col items-end justify-center'>

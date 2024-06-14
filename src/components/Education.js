2141906dc387bc0e692f7a7c990e2357ecd6cf57
Srijani Chakroborty <85583566+Srijani-Chakroborty@users.noreply.github.com>
2024-06-15 01:35:07 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -35,7 +35,7 @@ const Education = () => {
         </h2>
         <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
             <motion.div style={{scaleY:scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'/>
-            <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
+            <ul className='w-full flex flex-col items-start justify-between ml-4'>
                 <Details
                 type="Master Of Technology In Software Engineering"
                 time="2022-2024" place="Birla Institute of Sciend and Technology, Pilani (BITS, Pilani)"
